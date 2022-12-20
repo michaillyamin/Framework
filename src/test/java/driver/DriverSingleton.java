75a79c4737643a18d4438148aa41b120009f0e76
Michail Lyamin <lyamin0155@gmail.com>
2022-12-20 17:56:43 +0300

    trying parallel classes testing

--- src/test/java/driver/DriverSingleton.java
+++ src/test/java/driver/DriverSingleton.java
@@ -3,35 +3,46 @@ package driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class DriverSingleton {
-
-    private static WebDriver driver;
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
     private DriverSingleton() {}
 
     public static WebDriver getDriver() {
 
-        if (null == driver){
+        if (null == driver.get()) {
+            System.out.println("Driver Singleton. Thread id is: " + Thread.currentThread().getId());
             switch (System.getProperty("browser")) {
                 case "firefox": {
                     WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
+                    FirefoxOptions firefoxOptions = new FirefoxOptions();
+                    firefoxOptions.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
+                    driver.set(new FirefoxDriver());
                 }
                 default: {
                     WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--window-size=1920,1080",
+                            "--disable-extensions", "--proxy-server='direct://'", "--proxy-bypass-list=*", "--start-maximized",
+                            "--disable-gpu", "--ignore-certificate-errors","user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36");
+                    driver.set(new ChromeDriver());
+                    System.out.println("Driver. Thread id is: " + Thread.currentThread().getId());
                 }
             }
-            driver.manage().window().maximize();
+            driver.get().manage().window().maximize();
         }
-        return driver;
+        System.out.println("Driver Singleton outside. Thread id is: " + Thread.currentThread().getId());
+        return driver.get();
     }
 
-    public static void closeDriver() {
-        driver.quit();
-        driver = null;
+    public static void closeDriver(){
+        System.out.println("Close driver. Thread id is: " + Thread.currentThread().getId());
+        driver.get().quit();
+        driver.remove();
     }
     
 }
